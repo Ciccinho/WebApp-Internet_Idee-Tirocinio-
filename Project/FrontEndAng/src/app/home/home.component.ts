56385cd1182e977d42a307a20c73ff3300b4e8b8
Ciccinho <ciccinho07@gmail.com>
2025-04-07 16:44:05 +0200

    Commit07-04_16-44

--- Project/FrontEndAng/src/app/home/home.component.ts
+++ Project/FrontEndAng/src/app/home/home.component.ts
@@ -16,21 +16,39 @@ export class HomeComponent implements OnInit{
   user: string = '';
   token: string ='';
   anagrafica: AnagraficaComponent = new AnagraficaComponent;
-
+  errorMsg: String='';
+ 
   constructor(private authService: AuthService, private store: StorageService, ) {}
   
   ngOnInit(): void {
     if(this.store.isLogged()){
       this.token = this.store.getAuth();
       this.authService.getAnagraficaUser(this.token).subscribe({
-        next: (data)=>{ this.anagrafica = data;
+        next: (data)=>{ 
+          this.anagrafica = data;
+          this.errorMsg = '';
           console.log('data caricata in anagrafica: ', data);
         },
-        error: (error)=>{ console.error("Errore caricamento Utente", error);
+        error: (error)=>{ 
+          console.error("Errore caricamento Utente", error);
+          if (error.status === 0) {
+            this.errorMsg = 'Server non raggiungibile. Verifica la connessione.';
+          } else if (error.status === 401) {
+            this.errorMsg = 'Sessione scaduta. Effettua di nuovo il login.';
+            // Puoi anche fare redirect automatico al login se vuoi
+          } else if (error.status === 404) {
+            this.errorMsg = 'Dati utente non trovati.';
+          } else {
+            this.errorMsg = `Errore imprevisto (${error.status}): ${error.statusText}`;
+          }
         }
       });
+    } else {
+      this.errorMsg = 'Utente non autenticato. Effettua il login.';
     }
   }
+
+
 
   clearUserHome(): void {
     this.store.clean();
